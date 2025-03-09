77787d560d29d081d1ce154cde84d1291fdb36a0
TOP <goloshapov2016@gmail.com>
2025-03-09 12:45:43 +0300

    Сделано

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
+// Шаблон карточки, который используется для создания новых карточек
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// Контейнер, в который будут добавляться карточки
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция для создания новой карточки на основе переданных данных
+function createCard(cardData, deleteHandler) {
+  // Клонируем шаблон карточки
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  // Находим элементы внутри карточки и заполняем их данными
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  // Добавляем обработчик события для кнопки удаления карточки
+  deleteButton.addEventListener('click', () => deleteHandler(cardElement));
+  
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+// Функция для удаления карточки из DOM
+function deleteCard(card) { 
+  card.remove(); 
+}
+
+// Создаем и добавляем карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  cardContainer.append(cardElement);
+});
+
+
